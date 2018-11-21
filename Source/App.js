bcfbc86429c4995aab2132503ba43c697354f7ac
liuyibin23 <liuyibin23@163.com>
2018-11-21 12:00:36 +0800

    隐藏时间线控件和动画播放器控件

--- Source/App.js
+++ Source/App.js
@@ -11,7 +11,9 @@
     var viewer = new Cesium.Viewer('cesiumContainer', {
         scene3DOnly: true,
         selectionIndicator: false,
-        baseLayerPicker: false
+        baseLayerPicker: false,
+        animation: false,
+        timeline: false
     });
 
     //////////////////////////////////////////////////////////////////////////
